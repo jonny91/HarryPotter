29c396f5a90a3d6aea25059bc3db3c8483e4f88c
Jonny <jonny.hong91@gmail.com>
2017-07-06 19:07:49 +0800

    竖琴演示灯

---
+++ light/Main.c
@@ -0,0 +1,50 @@
+#include <STC89C5xRC.H>
+#include "BackRoom.h"
+
+void init()
+{
+	P00 = 1;
+	P01 = 1;
+	P02 = 1;
+	P03 = 1;
+	P04 = 1;
+	P05 = 1;
+	P06 = 1;
+	P07 = 1;
+}
+void main()
+{
+	init();
+	
+	//7个灯 每个灯亮1秒钟循环
+	while(1)
+	{
+		P00 = 0;
+		delay_ms(1000);
+		P00 = 1;
+		
+		P01 = 0;
+		delay_ms(1000);
+		P01 = 1;
+		
+		P02 = 0;
+		delay_ms(1000);
+		P02 = 1;
+		
+		P03 = 0;
+		delay_ms(1000);
+		P03 = 1;
+		
+		P04 = 0;
+		delay_ms(1000);
+		P04 = 1;
+		
+		P05 = 0;
+		delay_ms(1000);
+		P05 = 1;
+		
+		P06 = 0;
+		delay_ms(1000);
+		P06 = 1;
+	}
+}
